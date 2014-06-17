573ec7073806e3223830f8c0e6feebe0f19ee5e0
unknown <47518418G@VDI-JBOSS-W7-05.inspeccion.dom>
2014-06-17 12:42:21 +0200

    Borrar ficheros

--- js/backbone/collections/usuarios.js
+++
@@ -1,7 +0,0 @@
-AppUsuario.Collections.Usuarios = Backbone.Collection.extend({
-	url : 'http://localhost:9080/test/usuario/all',
-	model : AppUsuario.Models.Usuario,
-	initialize: function(){
-				console.info('Creado una collecion de usuarios');
-	}
-});
